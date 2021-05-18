51799ec95c0bd3b68db42596aa0c3d5aa7fa237c
Rosa Kang <therosessom@gmail.com>
2021-05-17 17:19:25 -0700

    coin board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css';
+import Coin from './Coin';
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -26,10 +28,33 @@ function App() {
   );
 
   return (
-     <div className='coin-app'>
-       
-
-     </div>
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
+    </div>
   );
 }
 
